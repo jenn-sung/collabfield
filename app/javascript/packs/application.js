a8a4ecb31102617d5ecd0b11a8c1c43208366d74
Jenn <jennsungdevelops@gmail.com>
2019-08-29 19:10:58 -0600

    add a basic navigation bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-//= require jquery
+//= require bootstrap
 //= require bootstrap-sprockets
+//= require jquery
+
